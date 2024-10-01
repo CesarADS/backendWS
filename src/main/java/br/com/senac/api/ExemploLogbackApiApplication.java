e9c8aff32d8cd5964c579b3572e24eb1c8673b04
CESAR AUGUSTO ALEXANDRE ALVES <cesar.alves@alunos.sc.senac.br>
2024-10-01 20:26:51 -0300

    atualizacoes andrei

--- src/main/java/br/com/senac/api/ExemploLogbackApiApplication.java
+++ src/main/java/br/com/senac/api/ExemploLogbackApiApplication.java
@@ -1,7 +1,14 @@
 package br.com.senac.api;
 
+import jakarta.jms.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 
 @SpringBootApplication
 public class ExemploLogbackApiApplication {
@@ -10,4 +17,28 @@ public class ExemploLogbackApiApplication {
 		SpringApplication.run(ExemploLogbackApiApplication.class, args);
 	}
 
+	// Os método Bean abaixo definem como as mensagens vão ser trafegadas no ActiveMQ, instancia automaticamente
+	@Bean
+	public JmsListenerContainerFactory<?> defaultFactory(
+			ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+
+		DefaultJmsListenerContainerFactory factory =
+				new DefaultJmsListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	// Tenta interpretar automaticamente as mensagens JSON, convertendo-as para um objeto
+	@Bean
+	public MappingJackson2MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter =
+				new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
 }
+
+
